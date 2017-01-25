abab15bcdf0de68349a0e4ed6128fd7d558a9639
Pavel <pavel@imac-pavel.capital-tour.lan>
2017-01-25 21:24:03 +0300

    Music

--- src/js/Maincontent.js
+++ src/js/Maincontent.js
@@ -3,19 +3,28 @@ import axios from 'axios'
 import '../css/Maincontent.css';
 
 let Articles = function(props) {
+    function playMusic() {
+        if (props.className === "article_right" && props.imageUrl === "content/images/cyberdj_240x320.png")
+        {
+            document.getElementById("2").src="content/images/cyberdj_240x320.gif";
+         document.getElementById("music").play()
+        }else {
+         document.getElementById("2").src="content/images/cyberdj_240x320.png";
+         document.getElementById("music").pause()
+            console.log(document.getElementById("2").src.indexOf("cyberdj_240x320.gif"))
+        }
+
+    }
+
     return (
         <div className="article_container">
-            <img className={props.className} alt="articleimg" src={props.imageUrl} role="presentation" />
+            <img id={props.Id} className={props.className} alt="articleimg" src={props.imageUrl} role="presentation" onClick={playMusic}/>
             <p className="articletext">{props.Text}</p>
         </div>
     )
 }
 
 let MainContent = React.createClass({
-    playMusic: function () {
-
-    },
-
     getInitialState: function() {
         return {
             article: []
@@ -35,7 +44,7 @@ let MainContent = React.createClass({
             <div>
                 <h1 className="maintext">Music is the breath of life!</h1>
                 {this.state.article.map(function(article) {
-                    return  <Articles key={article.id} imageUrl={article.imageUrl} Text={article.Text} className={article.class}/>
+                    return  <Articles key={article.id} imageUrl={article.imageUrl} Text={article.Text} className={article.class} Id={article.id}/>
                 })}
                 <audio controls id="music" src="content/Flatline.mp3" type="audio/mpeg"></audio>
             </div>
